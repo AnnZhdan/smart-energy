4882124a32728e909334105dd94876aca57a814a
Savinov94 <134193164+Savinov94@users.noreply.github.com>
2023-10-20 17:59:36 +0200

    4

--- src/js/rating.js
+++ src/js/rating.js
@@ -3,20 +3,27 @@ const starImages = document.querySelectorAll('.rating-container label svg use');
 
 ratingLabels.forEach((label, index) => {
   label.addEventListener('click', (event) => {
-    starImages.forEach((image, i) => {
+    for (let i = 0; i < ratingLabels.length; i++) {
       if (i <= index) {
-        image.setAttribute('xlink:href', './img/icon-sprite.svg#icon-Star-2');
+        starImages[i].setAttribute('xlink:href', './img/icon-sprite.svg#icon-Star-2');
       } else {
-        image.setAttribute('xlink:href', './img/icon-sprite.svg#icon-Star-1');
+        starImages[i].setAttribute('xlink:href', './img/icon-sprite.svg#icon-Star-1');
       }
-    });
+    }
   });
 
   label.addEventListener('focus', (event) => {
-    starImages[index].setAttribute('xlink:href', './img/icon-sprite.svg#icon-Star-2');
+    for (let i = 0; i <= index; i++) {
+      starImages[i].setAttribute('xlink:href', './img/icon-sprite.svg#icon-Star-2');
+    }
+    for (let i = index + 1; i < ratingLabels.length; i++) {
+      starImages[i].setAttribute('xlink:href', './img/icon-sprite.svg#icon-Star-1');
+    }
   });
 
   label.addEventListener('blur', (event) => {
-    starImages[index].setAttribute('xlink:href', './img/icon-sprite.svg#icon-Star-1');
+    for (let i = 0; i < ratingLabels.length; i++) {
+      starImages[i].setAttribute('xlink:href', './img/icon-sprite.svg#icon-Star-1');
+    }
   });
 });
